531575c38dc1af9d4c331da24238f1699d82988c
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-06-14 14:19:21 +0200

    Adding contracts

---
+++ src/test/resources/contracts/sum/given_when_emptyRequest_then_expectedResult.groovy
@@ -0,0 +1,31 @@
+package contracts.order
+
+import org.springframework.cloud.contract.spec.Contract
+
+/**
+ * Contract definition, written in a Groovy DSL
+ */
+Contract.make {
+
+    /**
+     * Request which completes with HTTP statusCode 200
+     */
+    request {
+        method 'POST'
+        url('/sum')
+        headers {
+            contentType(applicationJson())
+        }
+    }
+    response {
+        status 200
+        headers {
+            contentType(applicationJson())
+        }
+        body''' \
+{ 
+    "result": 0
+}
+'''
+    }
+}
