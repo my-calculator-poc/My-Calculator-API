a85a2151597772adf5b06d82e0011cf807f71666
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-06-14 12:15:53 +0200

    Adding initial Skeleton from https://start.spring.io/ Adding Swagger definition from https://editor.swagger.io/

---
+++ src/main/java/org/jab/microservices/myCalculator/MyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.jab.microservices.myCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCalculatorApplication.class, args);
+	}
+
+}
