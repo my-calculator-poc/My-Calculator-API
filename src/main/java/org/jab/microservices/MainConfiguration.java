30c36cf1f8a53b0df8c6853c5a6f139476bc4b82
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-08-13 16:38:24 +0200

    Upgrading SumServiceImpl, connecting with real My-Sum-API. Upgrading Test with the usage of Wiremock to avoid to run the real tests.

--- src/main/java/org/jab/microservices/MainConfiguration.java
+++ src/main/java/org/jab/microservices/MainConfiguration.java
@@ -2,9 +2,15 @@ package org.jab.microservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MainConfiguration {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
     @Bean
     SumService sumService() {
--- src/test/java/org/jab/microservices/SumControllerTest.java
+++ src/test/java/org/jab/microservices/SumControllerTest.java
@@ -1,6 +1,9 @@
 package org.jab.microservices;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,9 +28,31 @@ public class SumControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    WireMockServer wireMockServer;
+
+    @Before
+    public void setup () {
+        wireMockServer = new WireMockServer(9090);
+        wireMockServer.start();
+    }
+
+    @After
+    public void teardown () {
+        wireMockServer.stop();
+    }
+
+
+
     @Test
     public void given_SumWithPostAndFormData_whenMockMVC_thenResponseOKResult() throws Exception  {
 
+        //Given
+        wireMockServer.stubFor(com.github.tomakehurst.wiremock.client.WireMock.post(urlEqualTo("/sum"))
+                .willReturn(aResponse().withHeader("Content-Type", "application/json")
+                        .withStatus(200)
+                        .withBody("{\"result\":4}")));
+
+        //When
         Integer param1 = 1;
         Integer param2 = 3;
 
@@ -38,7 +65,10 @@ public class SumControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
         .content(jsonBodyRequest))
         .andDo(print())
+
+        //Then
         .andExpect(status().isOk())
         .andExpect(content().json(expectedJsonContent));
     }
+
 }
